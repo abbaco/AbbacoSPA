af7a20f37a80c9a0466cb0107eeca76bc2ffdb2b
Fernando <fernandobarcalarodriguez@gmail.com>
2020-07-01 18:40:59 +0200

    minor fixes

--- abbaco-spa/src/App.tsx
+++ abbaco-spa/src/App.tsx
@@ -3,12 +3,15 @@ import './App.css';
 import CashFlowsContainer from './pages/CashFlows.container';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import {faPlusCircle, faMinusCircle, faBalanceScale, faTrash, faArchive, faSave } from '@fortawesome/free-solid-svg-icons'
+import Container from 'react-bootstrap/Container';
 
 library.add(faPlusCircle, faMinusCircle, faBalanceScale, faTrash, faSave)
 
 function App() {
   return (
+    <Container className="col">
       <CashFlowsContainer/>
+    </Container>
   );
 }
 
